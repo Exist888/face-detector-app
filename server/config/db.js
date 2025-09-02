7d407b9c6fe5186c5c6a214e3865405e027bf861
Filip Herbst <filip_herbst@yahoo.com>
2025-09-01 21:10:45 -0500

    Added Supabase Certificate Authority

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,27 @@
 import knex from "knex";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+// Get the current file path
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name
+const __dirname = path.dirname(__filename);
+
+// Use SSL cert from Fly.io secrets, fallback to local file for dev
+const sslCert = process.env.SUPABASE_CA || fs.readFileSync(path.join(__dirname, "./certs/global-bundle.pem"), "utf-8");
 
 const db = knex({
     client: "pg",
     connection: {
         connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: false
+            rejectUnauthorized: true,
+            ca: sslCert
         }
     },
     pool: {
